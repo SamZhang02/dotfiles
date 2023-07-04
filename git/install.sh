e16624d6391e1c15ed0d96f8f31b4ce5e3574d5a
zhangzijing06 <zhangzijing06@meituan.com>
2023-07-04 10:19:47 +0800

    .

---
+++ git/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "SamZhang02"
+git config --global user.email "sam.zijing.zhang@gmail.com"
+git config --global core.editor "nvim"
+
+git config --global alias.log "log --oneline --decorate --graph"
